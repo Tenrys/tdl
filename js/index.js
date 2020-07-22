7e37dc8fa8d2c030895c15fc2c61551e795d0f54
Marceau Maubert <marceau.maubert2@gmail.com>
2020-07-22 03:31:06 +0200

    fix

--- js/index.js
+++ js/index.js
@@ -2,7 +2,8 @@ import IndexPage from "./pages/index";
 import UsersPage from "./pages/users";
 
 for (const [page, app] of Object.entries({ index: IndexPage, users: UsersPage })) {
-	const slug = location.pathname.match(/\/(.*).php$/)[1];
+	const slug = (location.pathname.match(/\/(.*).php$/) || [, "index"])[1];
+	console.log(slug);
 	if (slug == page && document.querySelector(`#${page}`)) {
 		app.$mount(`#${page}`);
 	}
